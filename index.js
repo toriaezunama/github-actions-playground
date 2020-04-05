7b942ea78c4e4233e4fd539a7ecb0b30b5d0b94f
Paul Wadsworth <paul@dmgblockchain.com>
2020-04-04 20:22:41 -0700

    Update index

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const fs = require("fs").promises;
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!\n"));
+// Read directory to see wether we are running production or development
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://0.0.0.0:${port}`)
-);
+(async () => {
+  const files = await fs.readdir(__dirname);
+  const prefix = files.includes("production") ? "production" : "development";
+
+  app.get("/", (_, res) => res.send(`${prefix} >> Hello World!\n`));
+
+  app.listen(port, () =>
+    console.log(`Example app listening at http://0.0.0.0:${port}`)
+  );
+})();
